1d5152be69c05abfbc94b7aac1c6818331ec8b6f
Michael Angelo Manaog <angelothegreat13@gmail.com>
2022-12-20 21:47:29 +0800

    dip start

---
+++ dip.php
@@ -0,0 +1,8 @@
+<?php
+/*
+    Dependency Inversion Principle(DIP)
+        - states that high level modules should not depend no low level modules; both should depend on abstractions.
+          Abstractions should not depend on details. Details should depend upon abstractions.
+
+
+*/

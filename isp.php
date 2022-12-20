8fea6b2659759c55f9a8cf3c41ac87b6caa3b809
Michael Angelo Manaog <angelothegreat13@gmail.com>
2022-12-20 21:32:38 +0800

    isp

--- isp.php
+++ isp.php
@@ -1,6 +1,28 @@
 <?php 
 /*
     Interface Segration Principle
-    
 
+    interface = contacts
+    segration = divide or split up
+    Interface Segration 
+        - "No client(class) should be forced to depend on methods it doesn't use"
+        - "No class should be forced to implement an interface that contractually binds that class to have a method it will never use"
 */
+
+interface File {
+    public function parse();
+    // public function htmlContent(); violates isp , so we need to segrate the interface
+}
+
+// interfaces are contracts that force classes to depend on a given method or methods
+interface Html {
+    public function htmlContent();
+}
+
+class Json implements File 
+{
+    public function parse()
+    {
+
+    }
+}

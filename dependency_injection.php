ab55b22dab20f1cf3e3a3ae4252a025bbc7189f3
Michael Angelo Manaog <angelothegreat13@gmail.com>
2022-12-23 20:41:57 +0800

    dpendency injection

---
+++ dependency_injection.php
@@ -0,0 +1,10 @@
+<?php 
+/*
+    Dependency Injection
+        - Injecting dependency
+        - Injecting class as parameter instead of instantiating it
+        - Decouples class construction from the given classes dependency constructions
+*/
+
+
+
--- dip.php
+++ dip.php
@@ -4,5 +4,19 @@
         - states that high level modules should not depend no low level modules; both should depend on abstractions.
           Abstractions should not depend on details. Details should depend upon abstractions.
 
+	1. What are higher level modules
+	2. What are lower level modules
+	3. What are the abstraction and how do we depend on them? 
 
+  	Higher Level
+    	- depends on abstraction via dependency injected parameter to function
+
+  	Lower Level
+    	- depends on abstraction via "implements" keyword
+
+	Interface (AKA Abstraction)
+
+	Service Container (AKA Dependency Injection Container)
+		- solution for class injected dependency has dependency
+		- auto wiring uses reflection
 */
